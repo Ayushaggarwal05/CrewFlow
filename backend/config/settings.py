fdf16249beee4e6bc64487decb3a34ff168819f4
Ayush Aggarwal <aayushaggarwal348@gmail.com>
2026-06-06 02:27:34 +0530

    Allow all CORS origins temporarily for debugging

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -167,6 +167,7 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
     default="http://localhost:5173,http://127.0.0.1:5173",
